9b9e5c002045ee6188ae267637440be751adcfe7
BoriskaKXL <xlboriska2015@gmail.com>
2019-08-30 20:15:11 +0400

    test

---
+++ lang.py
@@ -0,0 +1,81 @@
+import re
+
+f = open('title.txt', 'r')
+f = f.read()
+f = f.replace('...', '.')
+f = f.replace('.', ' .')
+reg = re.compile('[^a-zA-Zа-яА-Я. \n]')
+l = reg.sub('', f)
+l = l.split()
+for i in range(len(l)):
+    l[i] = l[i].lower()
+
+Dict = {}
+
+for i in range(len(l) - 1):
+    if l[i] not in Dict:
+        Dict[l[i]] = list()
+        Dict[l[i]].append(l[i + 1])
+    else:
+        if l[i + 1] not in Dict[l[i]]:
+            Dict[l[i]].append(l[i + 1])
+
+import random
+
+firstWord = ''
+
+
+def findFirst():
+    global firstWord
+    firstWord = l[(random.randint(0, len(l)))]
+    if firstWord == '.':
+        findFirst()
+
+
+def nextWord(x):
+    a = random.choice(Dict.get(x))
+    global firstWord
+    firstWord = a
+
+findFirst()
+D = ''
+D += firstWord.capitalize() + ' '
+u = open('new.txt', 'w')
+b = 0
+nextWord(firstWord)
+c = 0
+r = 0
+p = random.randint(2000,4000)
+for i in range(1, p):
+    if b == 1:
+        D += (' ' + firstWord.capitalize() + '')
+        b = 0
+    else:
+        D += (firstWord + '')
+    if firstWord == '.':
+        b = 1
+        firstWord = l[round(random.uniform(0, len(l)))]
+        c+=1
+        if c > 10:
+            r = random.randint(0,1)
+            if r == 1:
+                D += '\n'
+                c = 0
+                r = 0
+    else:
+        D += (' ')
+        nextWord(firstWord)
+
+
+D += '.'
+D = D.replace(' \n', '\n')
+D = D.replace('\n.', '\n')
+D = D.replace('\n. ', '\n')
+D = D.replace('\n. ', '\n')
+D = D.replace('\n ', '\n')
+D = D.replace('..', '.')
+D = D.replace('. .', '.')
+D = D.replace(' . ', '. ')
+D = D.replace(' .', '.')
+u = open('new.txt', 'w')
+u.write(D)
---
+++ world.py
@@ -0,0 +1,123 @@
+import random
+
+
+L = ['fish', 'shmp', 'rock', 'none']
+
+Ocean = []
+Ocean1 = []
+Ocean2 = []
+# raw = random.randint(3, 20)
+# col = random.randint(3, 20)
+
+
+raw = 30
+col = 30
+
+
+def show(H):
+    print('Океан')
+    for i in range(len(H)):
+        for j in range(len(H[i])):
+            print(H[i][j], end=' ')
+        print()
+
+
+for i in range(raw):
+    Ocean.append([])
+    for j in range(col):
+        Ocean[i].append(L[random.randint(0, 3)])
+
+for i in range(raw):
+    Ocean1.append([])
+    for j in range(col):
+        Ocean1[i].append([])
+
+def lifeCheck(i, j, ob):
+    if Ocean[i][j] == ob:
+        n = 0
+        if i + 1 != raw:
+            if Ocean[i + 1][j] == ob:
+                n += 1
+                # print('Есть сосед на нижней строке')
+        if i != 0:
+            if Ocean[i - 1][j] == ob:
+                n += 1
+                # print('Есть сосед на верхней строке')
+        if j + 1 != col:
+            if Ocean[i][j + 1] == ob:
+                n += 1
+                # print('Есть сосед справа')
+        if j != 0:
+            if Ocean[i][j - 1] == ob:
+                n += 1
+                # print('Есть сосед слева')
+        if (n == 4) or (n <= 1):
+            Ocean1[i][j] = 'none'
+        else:
+            Ocean1[i][j] = ob
+
+
+def rockCheck(i, j):
+    if Ocean[i][j] == 'rock':
+        Ocean1[i][j] = 'rock'
+
+
+def noneCheck(i, j):
+    if Ocean[i][j] == 'none':
+        s = 0
+        f = 0
+        if i + 1 != raw:
+            if Ocean[i + 1][j] == 'shmp':
+                s += 1
+        if i != 0:
+            if Ocean[i - 1][j] == 'shmp':
+                s += 1
+        if j + 1 != col:
+            if Ocean[i][j + 1] == 'shmp':
+                s += 1
+        if j != 0:
+            if Ocean[i][j - 1] == 'shmp':
+                s += 1
+        if i + 1 != raw:
+            if Ocean[i + 1][j] == 'fish':
+                f += 1
+        if i != 0:
+            if Ocean[i - 1][j] == 'fish':
+                f += 1
+        if j + 1 != col:
+            if Ocean[i][j + 1] == 'fish':
+                f += 1
+        if j != 0:
+            if Ocean[i][j - 1] == 'fish':
+                f += 1
+        if s == 3:
+            Ocean1[i][j] = 'shmp'
+        if f == 3:
+            Ocean1[i][j] = 'fish'
+        else:
+            Ocean1[i][j] = 'none'
+
+
+def fullCheck():
+    for i in range(raw):
+        for j in range(col):
+            lifeCheck(i, j, 'fish')
+            lifeCheck(i, j, 'shmp')
+            rockCheck(i, j)
+            noneCheck(i, j)
+    global Ocean
+    global Ocean1
+    global Ocean2
+    Ocean2 = Ocean
+    Ocean = Ocean1
+    show(Ocean1)
+
+
+
+
+
+show(Ocean)
+for i in range(5):
+    fullCheck()
+
+
